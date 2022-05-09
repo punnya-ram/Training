78477709ac090dccbd2f38c44e2060b5a9a775dd
Punnyaram.S <Punnyaram.S@EXIND3020-41.allfon.in>
2022-05-09 18:10:00 +0530

    spring---mvc

---
+++ spring-mvc/src/main/java/com/example/demo/controllers/WelcomController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/mvc")
+public class WelcomController {
+	
+	
+	@RequestMapping(value= "/greet",method= RequestMethod.GET)
+	@ResponseBody
+	public String getMessage() {
+		return "<h1 style='text-align:center'>Welcome to Spring MVC</h1>";
+	}
+
+	@RequestMapping(value= "/bye",method= RequestMethod.GET)
+	@ResponseBody
+	public String sendMessage() {
+		return "<h6 style='text-align:center'>Designed By Ramesh</h6>";
+	}
+
+}
